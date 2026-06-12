130caed39a3a63837f3ba39471dfe4b5cca3273b
JULES123e <elliot.fesquet@efrei.net>
2026-06-12 21:54:35 +0200

    reap

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.set_page_config(page_title="Mon Portfolio Data", page_icon="📊", layout="wide")
 
-st.title("Bonjour, je suis [Ton Nom] 👋")
+st.title("test analytics engineer")
 st.subheader("Data Scientist / Analyst / Développeur")
 
 st.write("Bienvenue sur mon portfolio réalisé avec Streamlit.")
