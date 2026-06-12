a7e0c5e5defe2a6f7e27e32aefd53efca5f6fc6e
JULES123e <elliot.fesquet@efrei.net>
2026-06-12 21:40:12 +0200

    Premier commit: Structure de base

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(page_title="Mon Portfolio Data", page_icon="📊", layout="wide")
+
+st.title("Bonjour, je suis [Ton Nom] 👋")
+st.subheader("Data Scientist / Analyst / Développeur")
+
+st.write("Bienvenue sur mon portfolio réalisé avec Streamlit.")
